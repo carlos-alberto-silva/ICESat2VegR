62dbabe0862040827d120e10dcb3dadadaaa0708
caiohamamura <caiohamamura@gmail.com>
2023-12-14 17:21:58 -0500

    * Make getBeams a method working for both ATL03 and ATL08

--- R/class.icesat2.R
+++ R/class.icesat2.R
@@ -1,6 +1,28 @@
 #' @importFrom hdf5r H5File
 setRefClass("H5File")
 
+# Base class for icesat H5 files
+setClass(
+  Class = "icesat2.h5",
+  slots = list(h5 = "H5File")
+)
+
+#' Class for ICESat-2 ATL03
+#'
+#' @slot h5 Object of class [`H5File`][hdf5r::H5File-class] from `hdf5r` package containing the
+#' ICESat-2 Global Geolocated Photon Data (ATL03)
+
+#'
+#' @seealso [`H5File`][hdf5r::H5File-class] in the `hdf5r` package and
+#' \url{https://icesat-2.gsfc.nasa.gov/sites/default/files/page_files/ICESat2_ATL03_ATBD_r006.pdf}
+#'
+#' @import methods
+#' @export
+icesat2.atl03_h5 <- setClass(
+  Class = "icesat2.atl03_h5",
+  contains = "icesat2.h5"
+)
+
 #' Class for ICESat-2 ATL08
 #'
 #' @slot h5 Object of class [`H5File`][hdf5r::H5File-class] from `hdf5r` package containing the
@@ -14,7 +36,7 @@ setRefClass("H5File")
 #' @export
 icesat2.atl08_h5 <- setClass(
   Class = "icesat2.atl08_h5",
-  slots = list(h5 = "H5File")
+  contains = "icesat2.h5"
 )
 
 
@@ -165,11 +187,6 @@ icesat2.atl08_h5 <- setClass(
   head(x@dt)
 }
 
-getBeams_validation <- function(h5) {
-  if (inherits(h5, what = c("icesat2.atl03_h5", "icesat2.atl08_h5")) == FALSE) {
-    stop("This is not an icesat2.atl03_h5 neither icesat2.atl08_h5")
-  }
-}
 
 #' Get Beams for icesat h5 classes
 #'
@@ -177,31 +194,18 @@ getBeams_validation <- function(h5) {
 #' @param ... Other parameters to dispatch to the hdf5 library
 #'
 #' @export
-getBeams <- function(x, ...) {
-  getBeams_validation(x)
-
-  groups <- x@h5$ls()$name
-  grep("gt[1-3][lr]", groups, value = TRUE)
-}
+setGeneric("getBeams", function(h5, ...) {
+  standardGeneric("getBeams")
+})
 
 
-#' @importFrom hdf5r H5File
-setRefClass("H5File")
-
-#' Class for ICESat-2 ATL03
-#'
-#' @slot h5 Object of class [`H5File`][hdf5r::H5File-class] from `hdf5r` package containing the
-#' ICESat-2 Global Geolocated Photon Data (ATL03)
-
-#'
-#' @seealso [`H5File`][hdf5r::H5File-class] in the `hdf5r` package and
-#' \url{https://icesat-2.gsfc.nasa.gov/sites/default/files/page_files/ICESat2_ATL03_ATBD_r006.pdf}
-#'
-#' @import methods
-#' @export
-icesat2.atl03_h5 <- setClass(
-  Class = "icesat2.atl03_h5",
-  slots = list(h5 = "H5File")
+setMethod(
+  "getBeams",
+  signature = c("icesat2.h5"),
+  function(h5, ...) {
+    groups <- h5@h5$ls()$name
+    grep("gt[1-3][lr]", groups, value = TRUE)
+  }
 )
 
 
@@ -210,7 +214,8 @@ setRefClass("data.table")
 
 #' Class for ATL08 attributes
 #'
-#' @slot data.table Object of class [`data.table`][data.table::data.table-class] from `data.table` package containing the
+#' @slot data.table Object of class [`data.table`][data.table::data.table-class]
+#' from `data.table` package containing the
 #' Extracted ICESat-2 ATL08 attributes
 
 #'
@@ -245,9 +250,9 @@ icesat2.atl03_dt <- setClass(
 
 #' Class for joined ATL03 and ATL08 attributes
 #'
-#' @slot data.table Object of class [`data.table`][data.table::data.table-class] from `data.table` package containing the
+#' @slot data.table Object of class [`data.table`][data.table::data.table-class]
+#' from `data.table` package containing the
 #' Joined ICESat-2 ATL03 and ATL08 attributes
-
 #'
 #' @seealso [`data.table`][data.table::data.table-class] in the `data.table` package and
 #' \url{https://icesat-2.gsfc.nasa.gov/sites/default/files/page_files/ICESat2_ATL03_ATBD_r006.pdf}
