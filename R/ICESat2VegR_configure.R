7ff1857d29c2fb5d014cc301fc7ad81458602174
caiohamamura <caiohamamura@gmail.com>
2024-04-09 17:32:20 -0400

    Initial vignettes

--- R/ICESat2VegR_configure.R
+++ R/ICESat2VegR_configure.R
@@ -1,10 +1,20 @@
 check_conda <- function() {
-    tryCatch(
-        {
-            reticulate::conda_version()
-        },
-        error = function(err) {}
-    )
+  tryCatch(
+    {
+      reticulate::conda_version()
+    },
+    error = function(err) {}
+  )
+}
+
+tryRestart <- function(auto = TRUE) {
+  resp <- readline("I will need to restart RSession, would you like to proceed? y/[n]")
+  if (substr(tolower(resp), 1, 1) == "y") {
+    if (require(rstudioapi)) {
+      try(rstudioapi::restartSession(), silent = TRUE)
+    }
+  }
+  message("Please restart your R environment manually and run this function again.")
 }
 
 #' Configure environment for using Google Earth Engine functions
@@ -18,46 +28,46 @@ check_conda <- function() {
 #' @return TRUE if everything works as expected, it is just an interactive installer
 #' @export
 ICESat2VegR_configure <- function() {
-    if (is.null(check_conda())) {
-        answer <- readline("Ananconda seems not to be available, would you like to install it? [y/N]: ")
-        if (tolower(answer) == "y") {
-            reticulate::install_miniconda()
-            message("Miniconda was successfully installed!")
-        }
+  if (is.null(check_conda())) {
+    answer <- readline("Ananconda seems not to be available, would you like to install it? [y/N]: ")
+    if (tolower(answer) == "y") {
+      reticulate::install_miniconda()
+      message("Miniconda was successfully installed!")
+    }
+  }
+
+  if (!is.null(check_conda())) {
+    reticulate::use_condaenv("r-reticulate")
+    if (reticulate::py_module_available("h5py") == FALSE) {
+      reticulate::py_install("h5py")
+    }
+    if (reticulate::py_module_available("h5py") == FALSE) {
+      message("h5py is not compatible with this version of Python, upgrading...")
+      reticulate::py_install("python")
+      tryRestart()
     }
 
-    if (!is.null(check_conda())) {
-        reticulate::use_condaenv("r-reticulate")
-      if (reticulate::py_module_available("h5py") == FALSE) {
-        reticulate::py_install("h5py")
-      }
-      if (reticulate::py_module_available("h5py") == FALSE) {
-        message("h5py is not compatible with this version of Python, upgrading...")
-        reticulate::py_install("python")
-        message("Please restart your R environment manually and run this function again.")
-      }
-
-      if (reticulate::py_module_available("earthaccess") == FALSE) {
-        reticulate::py_install("earthaccess")
-      }
-      if (reticulate::py_module_available("earthaccess") == FALSE) {
-        message("earthaccess is not compatible with this version of Python, upgrading...")
-        reticulate::py_install("python")
-        message("Please restart your R environment manually and run this function again.")
-      }
-      if (reticulate::py_module_available("ee") == FALSE) {
-        reticulate::py_install("earthengine-api")
-      }
-    } else {
-        warning("Something went wrong, conda is still not installed properly!")
+    if (reticulate::py_module_available("earthaccess") == FALSE) {
+      reticulate::py_install("earthaccess")
     }
-
-    if (!require(ICESat2VegR) || is.null(ee) || is.null(h5py) || is.null(earthaccess)) {
-        warning("The package could not be loaded properly please read the readme in")
-        warning("https://github.com/carlos-alberto-silva/ICESat2VegR and file a new issue")
-        warning("if the problem is not solved.")
-        return (FALSE)
-    } else {
-        return (TRUE)
+    if (reticulate::py_module_available("earthaccess") == FALSE) {
+      message("earthaccess is not compatible with this version of Python, upgrading...")
+      reticulate::py_install("python")
+      tryRestart()
     }
+    if (reticulate::py_module_available("ee") == FALSE) {
+      reticulate::py_install("earthengine-api")
+    }
+  } else {
+    warning("Something went wrong, conda is still not installed properly!")
+  }
+
+  if (!require(ICESat2VegR) || is.null(ee) || is.null(h5py) || is.null(earthaccess)) {
+    warning("The package could not be loaded properly please read the readme in")
+    warning("https://github.com/carlos-alberto-silva/ICESat2VegR and file a new issue")
+    warning("if the problem is not solved.")
+    return(FALSE)
+  } else {
+    return(TRUE)
+  }
 }
