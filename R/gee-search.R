337f0ed09e366c38b03c3b2eeab5889762abe9a5
caiohamamura <caiohamamura@gmail.com>
2024-04-15 19:07:27 -0400

    search_datasets: document and allow parameters for searching for gee datasets.

--- R/gee-search.R
+++ R/gee-search.R
@@ -8,29 +8,86 @@ unescape_html2 <- function(str) {
   strsplit(parsed, "#_|", fixed = TRUE)[[1]]
 }
 
+#' Search for Google Earth Engine datasets
+#'
+#' @param ... character arguments to search for within title and/or description
+#' @param title logical. Whether should search within the title, default TRUE.
+#' @param description logical. Whether should search within the description of the dataset, default TRUE.
+#' @param operator character. Should be either "OR" or "AND" to tell if the search needs to include
+#' all the queries "AND" or any of the queries "OR". Default "AND".
+#' 
+#' @return A `data.table` containing the id, title and description of the datasets that matched
+#' the supplied query ordered by relevance.
+#' 
 #' @export
-search_datasets <- function(..., refresh_cache = FALSE) {
+search_datasets <- function(..., title = TRUE, description = TRUE, operator = "and", refresh_cache = FALSE) {
   args <- list(...)
+  stopifnot("Operator should be either 'AND' or 'OR'" = tolower(operator) %in% c("and", "or"))
+  stopifnot(
+    "At least one of `title` or `description` should be TRUE" =
+      (inherits(title, "logical") && title[1]) ||
+        (inherits(description, "logical") && description[1])
+  )
 
   if (refresh_cache || is.null(ee_cache$search)) {
     "%>%" <- magrittr::"%>%"
     req <- httr2::request("https://developers.google.com/earth-engine/datasets/catalog") %>% httr2::req_perform()
     txt <- req %>% httr2::resp_body_string()
-    ids <- sub("ee-dataset[^a]+a href=\"/earth-engine/datasets/catalog/", "", regmatches(txt, gregexpr('ee-dataset[^a]+a href="(/earth-engine/datasets/catalog/[^"]+)', txt))[[1]])
-    title <- sub('a href="/earth-engine/datasets/catalog/.*data-text="', "", regmatches(txt, gregexpr('a href="/earth-engine/datasets/catalog/[^"]+.*?data-text="([^"]+)', txt))[[1]])
-    title <- unescape_html2(title)
-    description <- sub('ee-dataset-description-snippet">', "", regmatches(txt, gregexpr('ee-dataset-description-snippet">[^<]+', txt))[[1]])
-    description <- unescape_html2(description)
-    dt <- data.table::data.table(id = ids, title = title, description = description)
+
+    ids <- sub(
+      "ee-dataset[^a]+a href=\"/earth-engine/datasets/catalog/",
+      "",
+      regmatches(txt, gregexpr('ee-dataset[^a]+a href="(/earth-engine/datasets/catalog/[^"]+)', txt))[[1]]
+    )
+    the_title <- sub(
+      'a href="/earth-engine/datasets/catalog/.*data-text="',
+      "",
+      regmatches(txt, gregexpr('a href="/earth-engine/datasets/catalog/[^"]+.*?data-text="([^"]+)', txt))[[1]]
+    )
+    the_title <- unescape_html2(the_title)
+    the_description <- sub(
+      'ee-dataset-description-snippet">',
+      "",
+      regmatches(txt, gregexpr('ee-dataset-description-snippet">[^<]+', txt))[[1]]
+    )
+    the_description <- unescape_html2(the_description)
+
+    dt <- data.table::data.table(id = ids, title = the_title, description = the_description)
     ee_cache$search <- dt
   }
   dt <- ee_cache$search
+
+  # Search params
+  all_idx <- list()
   for (arg in args) {
-    dt <- dt[grep(tolower(arg), tolower(title))]
+    idx <- numeric()
+    if (title) {
+      idx <- grep(tolower(arg), tolower(dt$title))
+    }
+    if (description) {
+      idx <- union(idx, grep(tolower(arg), tolower(dt$description)))
+    }
+    all_idx[[""]] <- list(idx = idx)
   }
-  dt
+
+  n <- i <- NA
+  .N <- data.table::.N
+  dt_idx <- data.table::rbindlist(all_idx)[
+    , list(n = .N, i = min(.I)),
+    by = c("idx")
+  ][order(-n, i)]
+
+  if (tolower(operator) == "and") {
+    dt_idx <- dt_idx[n == length(args)]
+  }
+  dt[dt_idx$idx]
 }
 
+#' Retrieve the Google Earth Engine image catalog id
+#' 
+#' @param id character. The id retrieved from the data.table resulting from [`search_datasets()`].
+#' 
+#' @return The catalog id to open within Google Earth Engine.
 #' @export
 get_catalog_id <- function(id) {
   "%>%" <- magrittr::"%>%"
