de863d7e835b32106f2b733038a6f248215dc3b8
caiohamamura <caiohamamura@gmail.com>
2024-06-11 11:00:13 -0400

    Don't testthat cloud functions: depends on external web resources

--- tests/testthat/test-ICESat2.h5_cloud.R
+++ tests/testthat/test-ICESat2.h5_cloud.R
@@ -1,92 +1,92 @@
-lower_left_lon <- -85
-lower_left_lat <- 30.0
-upper_right_lon <- -84.0
-upper_right_lat <- 31.0
-version <- "006"
-short_name <- "ATL08"
-persist <- TRUE
-daterange <- c("2019-08-19", "2019-08-20")
-res <- NA
-atl08_h5 <- NA
-group <- NA
-ds <- NA
-vals <- NA
+# lower_left_lon <- -85
+# lower_left_lat <- 30.0
+# upper_right_lon <- -84.0
+# upper_right_lat <- 31.0
+# version <- "006"
+# short_name <- "ATL08"
+# persist <- TRUE
+# daterange <- c("2019-08-19", "2019-08-20")
+# res <- NA
+# atl08_h5 <- NA
+# group <- NA
+# ds <- NA
+# vals <- NA
 
 
-test_that("ATLAS_dataFinder_cloud works", {
-  res <<- ATLAS_dataFinder_cloud(
-    short_name,
-    lower_left_lon,
-    lower_left_lat,
-    upper_right_lon,
-    upper_right_lat,
-    version,
-    daterange,
-    persist
-  )
-  expect_equal(length(res), 1)
-})
+# test_that("ATLAS_dataFinder_cloud works", {
+#   res <<- ATLAS_dataFinder_cloud(
+#     short_name,
+#     lower_left_lon,
+#     lower_left_lat,
+#     upper_right_lon,
+#     upper_right_lat,
+#     version,
+#     daterange,
+#     persist
+#   )
+#   expect_equal(length(res), 1)
+# })
 
-test_that("Can create a ICESat2.h5_cloud", {
-  atl08_h5 <<- ICESat2.h5_cloud$new(h5 = res[1])
-  expect_true(inherits(atl08_h5, "ICESat2.h5_cloud"))
-})
+# test_that("Can create a ICESat2.h5_cloud", {
+#   atl08_h5 <<- ICESat2.h5_cloud$new(h5 = res[1])
+#   expect_true(inherits(atl08_h5, "ICESat2.h5_cloud"))
+# })
 
-test_that("Can list groups", {
-  expect_true("gt1l" %in% atl08_h5$ls())
-})
+# test_that("Can list groups", {
+#   expect_true("gt1l" %in% atl08_h5$ls())
+# })
 
-test_that("Can read attribute", {
-  message(atl08_h5$attr("short_name"))
-  expect_true(atl08_h5$attr("short_name") == "ATL08")
-})
+# test_that("Can read attribute", {
+#   message(atl08_h5$attr("short_name"))
+#   expect_true(atl08_h5$attr("short_name") == "ATL08")
+# })
 
-test_that("Can open group", {
-  group <<- atl08_h5[["gt1l"]]
-  expect_true(inherits(group, "ICESat2.h5_cloud"))
-})
+# test_that("Can open group", {
+#   group <<- atl08_h5[["gt1l"]]
+#   expect_true(inherits(group, "ICESat2.h5_cloud"))
+# })
 
-test_that("Exists work", {
-  expect_true(atl08_h5$exists("gt1l"))
-  expect_false(atl08_h5$exists("gt1s"))
-})
+# test_that("Exists work", {
+#   expect_true(atl08_h5$exists("gt1l"))
+#   expect_false(atl08_h5$exists("gt1s"))
+# })
 
-test_that("Group can also list groups", {
-  expect_true("land_segments" %in% group$ls())
-})
+# test_that("Group can also list groups", {
+#   expect_true("land_segments" %in% group$ls())
+# })
 
-test_that("Can open dataset from file", {
-  ds <<- atl08_h5[["gt1l/land_segments/latitude"]]
-  expect_true(inherits(ds, "ICESat2.h5ds_cloud"))
-})
+# test_that("Can open dataset from file", {
+#   ds <<- atl08_h5[["gt1l/land_segments/latitude"]]
+#   expect_true(inherits(ds, "ICESat2.h5ds_cloud"))
+# })
 
-test_that("Can open dataset from group", {
-  ds <<- group[["land_segments/latitude"]]
-  expect_true(inherits(ds, "ICESat2.h5ds_cloud"))
-})
+# test_that("Can open dataset from group", {
+#   ds <<- group[["land_segments/latitude"]]
+#   expect_true(inherits(ds, "ICESat2.h5ds_cloud"))
+# })
 
-test_that("Can find dataset dimension", {
-  expect_true(all(ds$dims > 0))
-})
+# test_that("Can find dataset dimension", {
+#   expect_true(all(ds$dims > 0))
+# })
 
-test_that("Can find open one value from dataset", {
-  val <- ds[1]
-  message(val)
-  expect_true(inherits(val, "numeric"))
-})
+# test_that("Can find open one value from dataset", {
+#   val <- ds[1]
+#   message(val)
+#   expect_true(inherits(val, "numeric"))
+# })
 
-test_that("Can find open five values from dataset", {
-  val <- ds[1:5]
-  message(paste(val, collapse = " "))
-  expect_true(length(val) == 5)
-})
+# test_that("Can find open five values from dataset", {
+#   val <- ds[1:5]
+#   message(paste(val, collapse = " "))
+#   expect_true(length(val) == 5)
+# })
 
-test_that("Can open entire datase", {
-  vals <<- ds[]
-  expect_true(length(vals) == ds$dims)
-})
+# test_that("Can open entire datase", {
+#   vals <<- ds[]
+#   expect_true(length(vals) == ds$dims)
+# })
 
-test_that("Can open by mask", {
-  vals2 <- ds[vals < mean(vals)]
-  expect_true(length(vals2) < length(vals))
-})
+# test_that("Can open by mask", {
+#   vals2 <- ds[vals < mean(vals)]
+#   expect_true(length(vals2) < length(vals))
+# })
--- tests/testthat/test-ICESat2.h5ds_compatibility.R
+++ tests/testthat/test-ICESat2.h5ds_compatibility.R
@@ -1,99 +1,99 @@
-lower_left_lon <- -85
-lower_left_lat <- 30.0
-upper_right_lon <- -84.0
-upper_right_lat <- 31.0
-version <- "006"
-short_name <- "ATL08"
-persist <- TRUE
-daterange <- c("2019-08-19", "2019-08-20")
-res <- NA
-atl08_h5 <- NA
-group <- NA
-ds <- NA
-vals <- NA
+# lower_left_lon <- -85
+# lower_left_lat <- 30.0
+# upper_right_lon <- -84.0
+# upper_right_lat <- 31.0
+# version <- "006"
+# short_name <- "ATL08"
+# persist <- TRUE
+# daterange <- c("2019-08-19", "2019-08-20")
+# res <- NA
+# atl08_h5 <- NA
+# group <- NA
+# ds <- NA
+# vals <- NA
 
 
-res <- ATLAS_dataFinder_cloud(
-  short_name,
-  lower_left_lon,
-  lower_left_lat,
-  upper_right_lon,
-  upper_right_lat,
-  version,
-  daterange,
-  persist
-)
+# res <- ATLAS_dataFinder_cloud(
+#   short_name,
+#   lower_left_lon,
+#   lower_left_lat,
+#   upper_right_lon,
+#   upper_right_lat,
+#   version,
+#   daterange,
+#   persist
+# )
 
-atl08_h5_cloud <- ICESat2.h5_cloud$new(h5 = res[1])
-h5_path <- Sys.getenv("ATL08_PATH")
-atl08_h5 <- ICESat2.h5_local$new(h5 = h5_path)
+# atl08_h5_cloud <- ICESat2.h5_cloud$new(h5 = res[1])
+# h5_path <- Sys.getenv("ATL08_PATH")
+# atl08_h5 <- ICESat2.h5_local$new(h5 = h5_path)
 
-test_that("Retrieve dataset dims", {
-  expect_true(
-    all.equal(
-      atl08_h5[["ancillary_data/control"]]$dims,
-      atl08_h5_cloud[["ancillary_data/control"]]$dims
-    )
-  )
+# test_that("Retrieve dataset dims", {
+#   expect_true(
+#     all.equal(
+#       atl08_h5[["ancillary_data/control"]]$dims,
+#       atl08_h5_cloud[["ancillary_data/control"]]$dims
+#     )
+#   )
 
-  land_segments <- atl08_h5[["/gt1l/land_segments"]]
-  land_segments_cloud <- atl08_h5_cloud[["/gt1l/land_segments"]]
-  expect_true(
-    all.equal(
-      land_segments[["delta_time"]]$dims,
-      land_segments_cloud[["delta_time"]]$dims
-    )
-  )
-})
-
-
-
-test_that("Get type are same", {
-  h5dtypes <- list()
-  h5dtypes[["ds_metrics"]] <- hdf5r::h5types$H5T_STD_I8LE
-  h5dtypes[["ds_surf_type"]] <- hdf5r::h5types$H5T_STD_I32LE
-  h5dtypes[["gt1l/land_segments/rgt"]] <- hdf5r::h5types$H5T_STD_I16LE
-  h5dtypes[["gt1l/land_segments/ph_ndx_beg"]] <- hdf5r::h5types$H5T_STD_I64LE
-  h5dtypes[["orbit_info/orbit_number"]] <- hdf5r::h5types$H5T_STD_U16LE
-  h5dtypes[["gt1l/land_segments/canopy/canopy_h_metrics"]] <- hdf5r::h5types$H5T_IEEE_F32LE
-  h5dtypes[["orbit_info/crossing_time"]] <- hdf5r::h5types$H5T_IEEE_F64LE
-
-  for (path in names(h5dtypes)) {
-    expect_equal(atl08_h5[[path]]$get_type(), atl08_h5_cloud[[path]]$get_type())
-  }
-})
-
-test_that("Chunk dimensions are same", {
-  chunk_dims_latitude <- atl08_h5[["gt2r/land_segments/latitude"]]$chunk_dims
-  chunk_dims_canopy <- atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims
+#   land_segments <- atl08_h5[["/gt1l/land_segments"]]
+#   land_segments_cloud <- atl08_h5_cloud[["/gt1l/land_segments"]]
+#   expect_true(
+#     all.equal(
+#       land_segments[["delta_time"]]$dims,
+#       land_segments_cloud[["delta_time"]]$dims
+#     )
+#   )
+# })
 
 
-  expect_true(
-    all.equal(
-      atl08_h5[["gt2r/land_segments/latitude"]]$chunk_dims,
-      atl08_h5_cloud[["gt2r/land_segments/latitude"]]$chunk_dims
-    )
-  )
 
-  expect_true(
-    all.equal(
-      atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims,
-      atl08_h5_cloud[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims
-    )
-  )
-})
+# test_that("Get type are same", {
+#   h5dtypes <- list()
+#   h5dtypes[["ds_metrics"]] <- hdf5r::h5types$H5T_STD_I8LE
+#   h5dtypes[["ds_surf_type"]] <- hdf5r::h5types$H5T_STD_I32LE
+#   h5dtypes[["gt1l/land_segments/rgt"]] <- hdf5r::h5types$H5T_STD_I16LE
+#   h5dtypes[["gt1l/land_segments/ph_ndx_beg"]] <- hdf5r::h5types$H5T_STD_I64LE
+#   h5dtypes[["orbit_info/orbit_number"]] <- hdf5r::h5types$H5T_STD_U16LE
+#   h5dtypes[["gt1l/land_segments/canopy/canopy_h_metrics"]] <- hdf5r::h5types$H5T_IEEE_F32LE
+#   h5dtypes[["orbit_info/crossing_time"]] <- hdf5r::h5types$H5T_IEEE_F64LE
 
-test_that("Can get fill value", {
+#   for (path in names(h5dtypes)) {
+#     expect_equal(atl08_h5[[path]]$get_type(), atl08_h5_cloud[[path]]$get_type())
+#   }
+# })
+
+# test_that("Chunk dimensions are same", {
+#   chunk_dims_latitude <- atl08_h5[["gt2r/land_segments/latitude"]]$chunk_dims
+#   chunk_dims_canopy <- atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims
+
+
+#   expect_true(
+#     all.equal(
+#       atl08_h5[["gt2r/land_segments/latitude"]]$chunk_dims,
+#       atl08_h5_cloud[["gt2r/land_segments/latitude"]]$chunk_dims
+#     )
+#   )
+
+#   expect_true(
+#     all.equal(
+#       atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims,
+#       atl08_h5_cloud[["gt2r/land_segments/canopy/canopy_h_metrics"]]$chunk_dims
+#     )
+#   )
+# })
+
+# test_that("Can get fill value", {
   
-  expect_equal(
-    atl08_h5[["gt2r/land_segments/terrain_flg"]]$get_fill_value(),
-    atl08_h5_cloud[["gt2r/land_segments/terrain_flg"]]$get_fill_value()
-  )
+#   expect_equal(
+#     atl08_h5[["gt2r/land_segments/terrain_flg"]]$get_fill_value(),
+#     atl08_h5_cloud[["gt2r/land_segments/terrain_flg"]]$get_fill_value()
+#   )
 
-  expect_true(
-    all.equal(
-      atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$get_fill_value(),
-      atl08_h5_cloud[["gt2r/land_segments/canopy/canopy_h_metrics"]]$get_fill_value()
-    )
-  )
-})
+#   expect_true(
+#     all.equal(
+#       atl08_h5[["gt2r/land_segments/canopy/canopy_h_metrics"]]$get_fill_value(),
+#       atl08_h5_cloud[["gt2r/land_segments/canopy/canopy_h_metrics"]]$get_fill_value()
+#     )
+#   )
+# })
