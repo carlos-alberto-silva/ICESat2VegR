0b68115e4c4cd45811665902d25ef0d31a03d1c1
Alvites Diaz <c.alvitesdiaz@ufl.edu>
2026-05-27 16:31:50 -0400

    update clip functions

---
+++ R/ICESat2VegR-package.R
@@ -0,0 +1,10 @@
+#' ICESat2VegR: NASA ICESat-2 data for land & vegetation
+#'
+#' Tools to download, read, process, model, and visualize ICESat-2 ATL03/ATL08
+#' for land and vegetation applications.
+#'
+#' @useDynLib ICESat2VegR, .registration = TRUE
+#' @import Rcpp Rdpack
+#' @importFrom Rdpack reprompt
+#' @importFrom Rcpp loadModule Module
+"_PACKAGE"
